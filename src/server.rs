280da4aa254623f6368d985db133034eee98303e
Thomas Heslin <13917706+tjheslin1@users.noreply.github.com>
2021-08-27 07:03:26 +0100

    Add Server with new fn

---
+++ src/server.rs
@@ -0,0 +1,25 @@
+#[derive(Debug)]
+pub struct Server {
+    name: String,
+}
+#[derive(Debug)]
+pub struct ServerCreationError {
+    pub message: String,
+}
+
+impl Server {
+    pub fn new(name: &str) -> Result<Server, ServerCreationError> {
+        match name {
+            server_name if server_name.len() > 63 => Err(ServerCreationError {
+                message: format!(
+                    "Error creating Server. Name needs to be 63 characters or fewer, \
+                        got: \"{}\".",
+                    server_name
+                ),
+            }),
+            server_name => Ok(Server {
+                name: server_name.to_string(),
+            }),
+        }
+    }
+}
---
+++ tests/server_tests.rs
@@ -0,0 +1,13 @@
+use irc_server::server::Server;
+
+#[test]
+fn test_server_creation_name_too_long() {
+    let server_creation_error =
+        Server::new("Areallylongnameforaserverthatjustoverexceedsthesixtyhreecharacterlimit")
+            .expect_err("Expected Err because Server name is too long.");
+
+    let expected_message = "Error creating Server. Name needs to be 63 characters or fewer, \
+        got: \"Areallylongnameforaserverthatjustoverexceedsthesixtyhreecharacterlimit\".";
+
+    assert_eq!(server_creation_error.message, expected_message);
+}
