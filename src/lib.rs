e9425f1797c2aade8734a845fef77d16d064c4b7
Thomas Heslin <13917706+tjheslin1@users.noreply.github.com>
2021-09-05 11:52:11 +0100

    Move server code from main.rs into tls_server.rs.
    Created tls_server_tests.rs to start testing handling different requests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod irc_server;
 pub mod threadpool;
+pub mod tls_server;
 pub mod user;
---
+++ tests/tls_server_tests.rs
@@ -0,0 +1,7 @@
+use irc_rust::tls_server;
+
+#[test]
+fn test_server_handle_http_formatted_request() {
+    // tls_server::handle_request(rc_server, buf);
+    todo!()
+}
